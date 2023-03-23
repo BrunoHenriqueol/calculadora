0c1b353ecd3c220555c1ef2efbca34f8c5908ee5
joao_v_muniz-silva <joao_v_muniz-silva@estudante.sesisenai.org.br>
2023-03-23 09:09:55 -0300

    layout somar

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,7 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtNmr1 = findViewById(R.id.txtNmr1);
-        txtNmr2 = findViewById(R.id.txtNmr1);
-        txtNmr2 = findViewById(R.id.txtNmr1);
+        txtNmr2 = findViewById(R.id.txtNmr2);
+        tvResultado = findViewById(R.id.tvResultado);
+    }
+    public void somar(View view) {
+        int valor1 = Integer.parseInt(txtNmr1.getText().toString());
+        int valor2 = Integer.parseInt(txtNmr2.getText().toString());
+
+        tvResultado.setText(String.valueOf(valor1 + valor2));
     }
 }
