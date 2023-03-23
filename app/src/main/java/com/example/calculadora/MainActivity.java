0944d9c07f3194a6bb0a1f62f29b7fc8dc983d0d
joao_v_muniz-silva <joao_v_muniz-silva@estudante.sesisenai.org.br>
2023-03-23 09:02:35 -0300

    dfag

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText txtNmr1;
+    private EditText txtNmr2;
+    private TextView tvResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtNmr1 = findViewById(R.id.txtNmr1);
+        txtNmr2 = findViewById(R.id.txtNmr1);
+        txtNmr2 = findViewById(R.id.txtNmr1);
     }
 }
