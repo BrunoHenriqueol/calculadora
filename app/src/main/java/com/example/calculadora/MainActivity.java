f65bfe7e8ae5029725d5c5ab7a97d96a9bb60afb
joao_v_muniz-silva <joao_v_muniz-silva@estudante.sesisenai.org.br>
2023-03-23 10:26:58 -0300

    layout

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -22,7 +22,24 @@ public class MainActivity extends AppCompatActivity {
     public void somar(View view) {
         int valor1 = Integer.parseInt(txtNmr1.getText().toString());
         int valor2 = Integer.parseInt(txtNmr2.getText().toString());
+        tvResultado.setText(String.valueOf(valor1 - valor2));
+    }
 
+    public void subtrair(View view) {
+        int valor1 = Integer.parseInt(txtNmr1.getText().toString());
+        int valor2 = Integer.parseInt(txtNmr2.getText().toString());
         tvResultado.setText(String.valueOf(valor1 + valor2));
+    }
+
+    public void Multiplicar(View view) {
+        int valor1 = Integer.parseInt(txtNmr1.getText().toString());
+        int valor2 = Integer.parseInt(txtNmr2.getText().toString());
+        tvResultado.setText(String.valueOf(valor1 / valor2));
+    }
+
+    public void Dividir(View view) {
+        int valor1 = Integer.parseInt(txtNmr1.getText().toString());
+        int valor2 = Integer.parseInt(txtNmr2.getText().toString());
+        tvResultado.setText(String.valueOf(valor1 * valor2));
     }
 }
